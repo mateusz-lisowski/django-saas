bc5a316339982d026863407524c69e1a8778c8d4
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-06-15 13:49:49 +0200

    Create tests for landing page

--- src/landing/tests.py
+++ src/landing/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from django.test import Client
+
+
+class LandingPageTest(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_landing_page_status_code(self):
+        """Test if landing page returns proper status code."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
