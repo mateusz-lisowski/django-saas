f3e0e0dbe8fcef553eaa52f6a8f39cd0cbcff9c9
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-07-10 14:52:23 +0200

    Complete subscriptions logic

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,12 +1,21 @@
+from django.contrib.auth.models import Group, Permission
 from django.db import models
+
+
+SUBSCRIPTION_PERMISSIONS = [
+    ("free", "Free tier permission"),
+    ("basic", "Basic tier permission"),
+    ("pro", "Pro tier permission"),
+]
 
 
 class Subscription(models.Model):
     name = models.CharField(max_length=256)
+    groups = models.ManyToManyField(Group)
+    permissions = models.ManyToManyField(
+        Permission,
+        limit_choices_to={"codename__in": [sub[0] for sub in SUBSCRIPTION_PERMISSIONS]}
+    )
 
     class Meta:
-        permissions = [
-            ("free", "Free tier permission"),
-            ("basic", "Basic tier permission"),
-            ("pro", "Pro tier permission"),
-        ]
+        permissions = SUBSCRIPTION_PERMISSIONS
--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def subscription_details(request):
+
+    if request.user.has_perm("subscriptions.basic"):
+        return render(request, "basic.html")
+
+    if request.user.has_perm("subscriptions.pro"):
+        return render(request, "pro.html")
+
+    return render(request, "free.html")
