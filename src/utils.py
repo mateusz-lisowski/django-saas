a78b63c851e5d5626a6bbfdffba169004ad3f3f5
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-07-11 13:34:25 +0200

    Create stripe customer app

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import stripe
+from decouple import config
+
+
+DJANGO_DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
+STRIPE_API_KEY = config("STRIPE_API_KEY", default="", cast=str)
+
+if "sk_test" in STRIPE_API_KEY and not DJANGO_DEBUG:
+    raise ValueError("Invalid stripe api key value. Should be production one.")
+
+
+stripe.api_key = STRIPE_API_KEY
+
+
+def create_stripe_customer():
+    pass
+
