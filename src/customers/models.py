a313ddee0f5cde804a46c737bf31bedc7269e3da
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-07-11 13:40:28 +0200

    Create and register stripe customer model

--- src/customers/models.py
+++ src/customers/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    stripe_id = models.CharField(max_length=64, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}"
