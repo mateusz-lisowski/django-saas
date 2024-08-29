044f8ff67cdb52cd1ca3430bf6c66818b98cceca
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-08-29 22:43:11 +0200

    Prepare subscription view

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -4,11 +4,4 @@ from django.shortcuts import render
 
 @login_required
 def subscription_details(request):
-
-    if request.user.has_perm("subscriptions.basic"):
-        return render(request, "basic.html")
-
-    if request.user.has_perm("subscriptions.pro"):
-        return render(request, "pro.html")
-
-    return render(request, "free.html")
+    return None
